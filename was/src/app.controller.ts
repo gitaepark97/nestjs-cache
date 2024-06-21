41d3cb1a5688aa788566daea9ac7561173289e70
gitaepark97 <parkkitae7@gmail.com>
2024-06-21 16:03:33 +0900

    feat: cache 적용

--- was/src/app.controller.ts
+++ was/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @UseInterceptors(CacheInterceptor)
+  @CacheKey('some_route')
+  @CacheTTL(6000)
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
     return this.appService.getHello();
   }
 }
--- was/src/app.module.ts
+++ was/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { redisStore } from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register(<RedisClientOptions>{
+      store: redisStore,
+      socket: {
+        host: 'cache',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
